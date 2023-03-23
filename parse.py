f87b7b1df94cd25ebe870ad7e7a972ecaa1521b6
Michael French <michael.x.french@gmail.com>
2023-03-23 16:02:36 -0500

    repo setup

--- parse.py
+++ parse.py
@@ -5,7 +5,15 @@ import datetime
 from typing import List
 
 
-def parse_endsong(filename: str):
+def parse_endsong(filename: str) -> List[dict]:
+    """Given a filename, return the processed song data
+
+    Args:
+        filename (str): Name of the file to parse
+
+    Returns:
+        List[dict]: JSON data from the file
+    """
     with open(filename, "r") as f:
         data = json.load(f)
 
@@ -21,13 +29,21 @@ def parse_endsong(filename: str):
         song["track"] = song.pop("master_metadata_track_name")
         song["artist"] = song.pop("master_metadata_album_artist_name")
         song["album"] = song.pop("master_metadata_album_album_name")
-        
+
         new_data.append(song)
 
     return new_data
 
 
-def parse_all_files(folder: str):
+def parse_all_files(folder: str) -> List[dict]:
+    """Given a folder name, iterate through all files and return the processed song data
+
+    Args:
+        folder (str): name of a folder containg .json files with song data
+
+    Returns:
+        List[dict]: JSON data from all files in the folder
+    """
     all_songs = []
     for filename in os.listdir(folder):
         if re.match(r"^(endsong).*\.(json)$", filename):
@@ -36,14 +52,14 @@ def parse_all_files(folder: str):
     return sorted(all_songs, key=lambda k: k["ts"])
 
 
-def export(data: List):
+def export(data: List[dict]) -> None:
+    """Export song data into a new JSON file
+
+    Args:
+        data (List): List of song data
+    """
     for song in data:
         song.pop("date")
 
     with open("export.json", "w") as f:
         json.dump(data, f, indent=4)
-
-
-if __name__ == "__main__":
-    songs = parse_all_files("MyData")
-    export(songs)
